8662f6ea0191d7df281737aac43a3c5d306c5aa5
jamal-Bahammou <jamalbahammou456@gmail.com>
2020-07-10 16:13:18 +0100

    Todo Controller & database

---
+++ database/migrations/2020_07_10_141009_create_todos_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTodosTable extends Migration
+{
+
+    public function up() {
+        Schema::create('todos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->text('description');
+            $table->boolean('completed');
+            $table->timestamps();
+        });
+    }
+
+    public function down() {
+        Schema::dropIfExists('todos');
+    }
+}
